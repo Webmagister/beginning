c8877c3795749749d78c7883de706dcab8e1ed7b
Pavel Ataykin <pavel.ataykin@gmail.com>
2017-10-01 13:23:15 +0300

    Сделал движение полярной розы по кругу.

--- sfml2.2/main.cpp
+++ sfml2.2/main.cpp
@@ -7,35 +7,34 @@
 constexpr unsigned WINDOW_WIDTH = 800;
 constexpr unsigned WINDOW_HEIGHT = 600;
 
-float toDegrees(float radius)
-{
-    return float(double(radius) * 180.0 / M_PI);
-}
-
 int main()
 {
-    constexpr int pointCount = 200;
+    constexpr int pointCount = 300;
+    constexpr int radius = 40;
+    constexpr float period = 3.f;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 4;
     sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Polar Rose", sf::Style::Default, settings);
     sf::Clock clock;
 
-    sf::ConvexShape ellipse;
-    ellipse.setPosition({400, 320});
-    ellipse.setFillColor(sf::Color(0xFF, 0x09, 0x80));
+    sf::ConvexShape rose;
+    rose.setPosition({500, 300});
+    rose.setFillColor(sf::Color(0xFF, 0x09, 0x80));
 
-    sf::Vector2f position = {0, 0};
+    sf::Vector2f position = {500, 320};
     const float speed = 20.f;
 
-    ellipse.setPointCount(pointCount);
+    const sf::Vector2f center = {400, 300};
+
+    rose.setPointCount(pointCount);
     for (int pointNo = 0; pointNo < pointCount; ++pointNo)
     {
         float angle = float(2 * M_PI * pointNo) / float(pointCount);
         sf::Vector2f point = {
             200 * std::sin(6 * angle) * std::sin(angle),
             200 * std::sin(6 * angle) * std::cos(angle)};
-        ellipse.setPoint(pointNo, point);
+        rose.setPoint(pointNo, point);
     }
 
     while (window.isOpen())
@@ -49,19 +48,15 @@ int main()
             }
         }
 
-        const float dt = clock.restart().asSeconds();
+        float time = clock.getElapsedTime().asSeconds();
 
-        position = ellipse.getPosition();
+        position.x = radius * cos((2 * M_PI * time) / period);
+        position.y = radius * sin((2 * M_PI * time) / period);
 
-        const float angle = atan2(position.y, position.x);
-
-        position.x += dt * 20 * std::cos(toDegrees(angle));
-        position.y += dt * 20 * std::sin(toDegrees(angle));
-
-        ellipse.setPosition(position);
+        rose.setPosition(center + position);
 
         window.clear();
-        window.draw(ellipse);
+        window.draw(rose);
         window.display();
     }
 }
